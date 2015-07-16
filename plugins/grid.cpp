6b53325df7b5330cd649a84a010b23037621d43a
ammen99 <loopme99@hotmail.com>
2015-07-17 00:43:12 +0300

    Added animation to grid plugin

--- plugins/grid.cpp
+++ plugins/grid.cpp
@@ -1,11 +1,15 @@
 #include "../include/core.hpp"
 #include <algorithm>
 
+#define GetProgress(start,end,curstep,steps) (((end)*(curstep)+(start) \
+                                            *((steps)-(curstep)))/(steps))
+
 struct GridWindow {
-    Window id;
+    FireWindow win;
     XWindowAttributes size;
-    GridWindow(Window id, int x, int y, int w, int h) {
-        this->id = id;
+    GridWindow(){}
+    GridWindow(FireWindow id, int x, int y, int w, int h) {
+        this->win = id;
         size.x = x;
         size.y = y;
         size.width  = w;
@@ -19,7 +23,18 @@ class Grid : public Plugin {
     KeyBinding keys[10];
     KeyCode codes[10];
 
+    Hook rnd;
+    int steps;
+    int curstep;
+    GridWindow currentWin;
+
+    public:
+    Grid() {
+    }
+
     void init() {
+        options.insert(newIntOption("duration", 200));
+
         codes[1] = XKeysymToKeycode(core->d, XK_KP_End);
         codes[2] = XKeysymToKeycode(core->d, XK_KP_Down);
         codes[3] = XKeysymToKeycode(core->d, XK_KP_Page_Down);
@@ -40,33 +55,96 @@ class Grid : public Plugin {
             keys[i].type   = BindingTypePress;
             core->addKey(&keys[i], true);
         }
+
+        rnd.action = std::bind(std::mem_fn(&Grid::step), this);
+        core->addHook(&rnd);
     }
 
-    void toggleMaxim(FireWindow win) {
+    void initOwnership(){
+        owner->name = "grid";
+        owner->compatAll = false;
+    }
+
+    void updateConfiguration() {
+        steps = getSteps(options["duration"]->data.ival);
+    }
+
+    void step() {
+
+        GetTuple(sw, sh, core->getScreenSize());
+        float offx = GetProgress(0,
+                currentWin.win->attrib.x - currentWin.size.x, curstep, steps);
+
+        float offy = GetProgress(0,
+                currentWin.win->attrib.y - currentWin.size.y, curstep, steps);
+
+        offx /= (sw / 2.);
+        offy /= (sh / 2.);
+
+        float sclx = GetProgress(currentWin.win->attrib.width,
+                           currentWin.size.width, curstep, steps);
+
+        float scly = GetProgress(currentWin.win->attrib.height,
+                           currentWin.size.height, curstep, steps);
+
+        sclx /= currentWin.win->attrib.width;
+        scly /= currentWin.win->attrib.height;
+
+
+        float w2 = float(sw) / 2.;
+        float h2 = float(sh) / 2.;
+
+        float tlx = float(currentWin.win->attrib.x) - w2,
+              tly = h2 - float(currentWin.win->attrib.y);
+
+        float ntlx = sclx * tlx;
+        float ntly = scly * tly;
+
+        offx -= float(tlx - ntlx) / w2;
+        offy += float(tly - ntly) / h2;
+
+        currentWin.win->transform.translation =
+            glm::translate(glm::mat4(), glm::vec3(-offx, offy, 0));
+        currentWin.win->transform.scalation =
+            glm::scale(glm::mat4(), glm::vec3(sclx, scly, 0));
+
+        curstep++;
+        if(curstep == steps) {
+            currentWin.win->transform.translation = glm::mat4();
+            currentWin.win->transform.scalation = glm::mat4();
+
+            WinUtil::moveWindow(currentWin.win,
+                    currentWin.size.x, currentWin.size.y);
+            WinUtil::resizeWindow(currentWin.win,
+                    currentWin.size.width, currentWin.size.height);
+            core->setRedrawEverything(false);
+            core->dmg = core->getMaximisedRegion();
+            rnd.disable();
+        }
+    }
+
+    void toggleMaxim(FireWindow win, int &x, int &y, int &w, int &h) {
         auto it = std::find_if(wins.begin(), wins.end(),
                 [win](GridWindow w) {
-                return win->id == w.id;
+                return win->id == w.win->id;
                 });
 
         if(it == wins.end()) { // we haven't maximized this window, so
             // maximize it and add it to window list
             std::cout << "maximizing window";
-            wins.push_back(GridWindow(win->id, win->attrib.x, win->attrib.y,
+            wins.push_back(GridWindow(win, win->attrib.x, win->attrib.y,
                         win->attrib.width, win->attrib.height));
 
-            auto t = core->getScreenSize();
-            auto w = std::get<0> (t);
-            auto h = std::get<1> (t);
+            GetTuple(sw, sh, core->getScreenSize());
 
-            WinUtil::moveWindow(win, 0, 0);
-            WinUtil::resizeWindow(win, w, h);
+            x = y = 0, w = sw, h = sh;
             return;
         }
 
         // we have already maximized window, so restore its previous state
-        GridWindow w = *it;
-        WinUtil::moveWindow(win, w.size.x, w.size.y);
-        WinUtil::resizeWindow(win, w.size.width, w.size.height);
+        GridWindow gwin = *it;
+        x = gwin.size.x, y = gwin.size.y;
+        w = gwin.size.width, h = gwin.size.height;
         wins.erase(it); // and remove it so we can maximize it again
     }
 
@@ -101,18 +179,24 @@ class Grid : public Plugin {
         if(!win)
             return;
 
+        int x, y, w, h;
         for(int i = 1; i < 10; i++) {
             if(ctx->xev.xkey.keycode == codes[i]) {
                 if(i == 5)
-                    toggleMaxim(win);
-                else {
-                    int x, y, w, h;
+                    toggleMaxim(win, x, y, w, h);
+                else
                     getSlot(i, x, y, w, h);
-                    WinUtil::moveWindow  (win, x, y);
-                    WinUtil::resizeWindow(win, w, h);
-                }
             }
         }
+
+        currentWin.win = win;
+        currentWin.size.x = x;
+        currentWin.size.y = y;
+        currentWin.size.width = w;
+        currentWin.size.height = h;
+        curstep = 0;
+        rnd.enable();
+        core->setRedrawEverything(true);
     }
 };
 
